2701bd6bcdf4e0c60545450b6dd7fe7342ee473b
Mathias hendrikx <mathias_hendrikx@hotmail.com>
2019-09-11 13:21:04 +0200

    update

---
+++ public/public/read.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Function to query information based on 
+ * a parameter: in this case, location.
+ *
+ */
+
+require "../config.php";
+require "../common.php";
+
+if (isset($_POST['submit'])) {
+  if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
+
+  try  {
+    $connection = new PDO($dsn, $username, $password, $options);
+
+    $sql = "SELECT * 
+            FROM car
+            WHERE brand = :brand";
+
+    $location = $_POST['brand'];
+    $statement = $connection->prepare($sql);
+    $statement->bindParam(':brand', $location, PDO::PARAM_STR);
+    $statement->execute();
+
+    $result = $statement->fetchAll();
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+?>
+<?php require "templates/header.php"; ?>
+        
+<?php  
+if (isset($_POST['submit'])) {
+  if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Brand</th>
+          <th>Model</th>
+        </tr>
+      </thead>
+      <tbody>
+      <?php foreach ($result as $row) : ?>
+        <tr>
+          <td><?php echo escape($row["id"]); ?></td>
+          <td><?php echo escape($row["brand"]); ?></td>
+          <td><?php echo escape($row["model"]); ?></td>
+        </tr>
+      <?php endforeach; ?>
+      </tbody>
+    </table>
+    <?php } else { ?>
+      <blockquote>No results found for <?php echo escape($_POST['location']); ?>.</blockquote>
+    <?php } 
+} ?> 
+
+<h2>Search for a brand</h2>
+
+<form method="post">
+  <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
+  <label for="brand">Brand</label>
+  <input type="text" id="brand" name="brand">
+  <input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
